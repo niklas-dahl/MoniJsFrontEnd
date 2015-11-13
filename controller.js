88b4ddb514f9a86b96748e9ff907b0e9344bc796
Niklas <Niklas.Dahl@inform-software.com>
2015-11-13 16:45:27 +0100

    Week selection

--- controller.js
+++ controller.js
@@ -1,5 +1,28 @@
+
 var app = angular.module("app", []);
 
 app.controller("indexCtrl", function($scope) {
-	$scope.name = "Niklas";
+	var now = moment();
+	$scope.week = now.week();
+
+	$scope.getCurrentDays = function() {
+		var days = [];
+		var weekDate = moment().week($scope.week).startOf("isoweek");
+		for(i=0; i < 7; i++) {
+			days.push(weekDate.format("dddd D. MMMM"));
+			weekDate.add(1, "day")
+		}
+		return days;
+	}
+
+	$scope.addWeek = function(n) {
+		$scope.week += n;
+	}
+	$scope.subWeek = function(n) {
+		$scope.week -= n;
+	}
+	$scope.getDate = function() {
+		return moment().week($scope.week).format("W, MMMM");
+	}
+
 });
