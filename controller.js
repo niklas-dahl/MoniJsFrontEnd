2b70a276e1673d730f4fe90356a597bf0400874a
Niklas Dahl <niklasdahl97@gmail.com>
2015-11-14 18:19:55 +0100

    Made some changes ;D

--- controller.js
+++ controller.js
@@ -4,22 +4,36 @@ var app = angular.module("app", []);
 app.controller("indexCtrl", function($scope) {
 	var now = moment();
 	$scope.week = now.week();
+	currentWeekDays = [];
+	updateCurremtWeekDays();
 
 	$scope.getCurrentDays = function() {
-		var days = [];
+		return currentWeekDays;
+	}
+	
+	function updateCurremtWeekDays() {
+		currentWeekDays = [];
 		var weekDate = moment().week($scope.week).startOf("isoweek");
 		for(i=0; i < 7; i++) {
-			days.push(weekDate.format("dddd D. MMMM"));
+			var dayInfo = {
+				dayName: weekDate.format("dddd"),
+				day: weekDate.format("DD"),
+				monthName: weekDate.format("MMMM"),
+				month: weekDate.format("M"),
+				wholeDate: weekDate.format()
+			}
+			currentWeekDays.push(dayInfo);
 			weekDate.add(1, "day")
 		}
-		return days;
 	}
 
 	$scope.addWeek = function(n) {
 		$scope.week += n;
+		updateCurremtWeekDays();
 	}
 	$scope.subWeek = function(n) {
 		$scope.week -= n;
+		updateCurremtWeekDays();
 	}
 	$scope.getDate = function() {
 		return moment().week($scope.week).format("W, MMMM");
