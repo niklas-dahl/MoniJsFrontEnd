2f3955b5510eb401836da429002b2fc0574ac604
niklas-dahl <niklasdahl97@gmail.com>
2015-12-11 13:57:31 +0100

    Change to Jade removed unessesary files

--- staticServer.js
+++ staticServer.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var app = express();
+var jade = require('jade');
+
+/*app.get('/', function (req, res) {
+  res.send(jade.renderFile('index.jade'));
+});*/
+
+app.set('view engine', 'jade');
+
+app.get('/', function (req, res) {
+  res.render('index');
+});
 
 app.use(express.static('./'));
 
@@ -7,5 +18,5 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', "localhost", port);
+  console.log('Static server listening at http://%s:%s', "localhost", port);
 });
